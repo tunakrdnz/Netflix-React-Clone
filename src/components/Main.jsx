3da63ad7029fd9b29672050af61c037a33a6ee2a
Tuna <tunakrdnz@gmail.com>
2023-05-11 23:07:41 +0300

    added some tailwind css to main.jsx file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,13 +16,13 @@ export default function Main() {
             <form action="">
                 <h3 className='text-lg font-normal'>Ready to watch? Enter your email to create or restart your membership.</h3>
                 <div className='flex flex-row items-start text-left relative w-full m-4'>
-                    <div>
-                        <label htmlFor="email-area">Email address</label>
-                        <div>
-                          <input type="email" id="email-area" name="email" />
+                    <div className='flex flex-auto relative flex-wrap align-text-top w-auto '>
+                        <label className='absolute block z-[1] whitespace-nowrap overflow-hidden text-slate-700 leading-6 inset-x-4 top-4' htmlFor="email-area">Email address</label>
+                        <div className='fill-current  font-normal text-base p-0 min-w-[200px] w-full color text-white flex items-center text-left relative gap-0.5 leading-[100%] z-0'>
+                          <input  className='w-full text-transparent leading-6 min-h-[16px] min-w-[16px] bg-transparent border-solid border-0 border-transparent' type="email" id="email-area" name="email" />
                         </div>
                     </div>
-                    <button className=' flex items-center m-0 justify-center rounded cursor-pointer relative w-auto align-text-top text-2xl font-medium h-14 bg-red-600 overflow-visible hover:bg-red-700 px-3 duration-200'>
+                    <button className=' flex items-center m-0 justify-center rounded cursor-pointer relative w-auto align-text-top text-2xl font-medium h-14 bg-red-600 overflow-visible hover:bg-red-700 px-3 min-h-[56px] duration-200'>
                         Get Started
                     </button>
                 </div>
